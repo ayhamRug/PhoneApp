93138365f0367e4dabba4a4962a8e8c9aca93fd8
Ayham <aylosa@outlook.com>
2018-12-03 18:18:10 +0100

    Ayham: First commit

---
+++ src/MainScreen.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+
+public class MainScreen {
+
+    private JPanel appView;
+    private JLabel appTitle;
+    private JTextField textFieldMax;
+    private JTextField textFieldMin;
+    private JLabel incomeTitle;
+    private JLabel incomeMin;
+    private JLabel incomeMax;
+    private JButton submit;
+
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame("Phone App");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setContentPane(new MainScreen().appView);
+        frame.pack();
+        frame.setSize(1200,800);
+        frame.setVisible(true);
+
+    }
+
+
+}
+
+
