91262cdb3cde90e32cbe60280324b8bc3aaaf0f5
Glebas <glebko21@gmail.com>
2018-12-13 19:43:55 +0100

    created a TODO file

---
+++ src/TODO.java
@@ -0,0 +1,11 @@
+//
+// class to hold TODO items. should be removed before final submission.
+//
+public class TODO {
+
+    //TODO: fill Preferable brand from PhoneBrandSelector enum instead of just hardcoding values
+    //TODO: add more options for filtering, eg.: touchscreen/buttons, memory capacity and etc.
+    //TODO: improve interface(controls location, design)
+    //TODO: filtering by performance, design, battery life, camera
+    //TODO: ageSuitabilityScore, designScore, performanceScore, batteryScore, cameraScore should be measured equally, for eg. from 1 to 10.
+}
